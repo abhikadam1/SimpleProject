c264cf322733c6d63b051c627c7320be628cc637
Abhi Kadam <kadamabhishek3188@gmail.com>
2025-08-19 23:41:38 +0530

    Crud JPA Changes

--- src/main/java/com/example/SimpleProject/Service/ProductService.java
+++ src/main/java/com/example/SimpleProject/Service/ProductService.java
@@ -1,6 +1,8 @@
 package com.example.SimpleProject.Service;
 
 import com.example.SimpleProject.Model.Product;
+import com.example.SimpleProject.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,51 +11,62 @@ import java.util.List;
 @Service
 public class ProductService
 {
-    List<Product> products = new ArrayList<>( Arrays.asList(
-            new Product(101, "Iphone", 150000),
-            new Product(102, "One Plus", 45000),
-            new Product(103, "Nothing Phone", 35000),
-            new Product(104, "Samsung", 115000)
-        ));
+//    List<Product> products = new ArrayList<>( Arrays.asList(
+//            new Product(101, "Iphone", 150000),
+//            new Product(102, "One Plus", 45000),
+//            new Product(103, "Nothing Phone", 35000),
+//            new Product(104, "Samsung", 115000)
+//        ));
+    @Autowired
+    ProductRepository prodRepo;
+
     public List<Product> getProducts()
     {
-            return  products;
+//            return  products;
+        return prodRepo.findAll();
     }
 
     public Product getProductById(int prodId) {
-         return products.stream()
-                 .filter(p -> p.getProductId() == prodId)
-                 .findFirst().orElse(new  Product(0, "No Product Found", 0));
+//         return products.stream()
+//                 .filter(p -> p.getProductId() == prodId)
+//                 .findFirst().orElse(new  Product(0, "No Product Found", 0));
+        return prodRepo.findById(prodId).orElse(new Product());
     }
 
     public void addProduct(Product prod)
     {
-        products.add(prod);
+//        products.add(prod);
+        prodRepo.save(prod);
     }
     public Product updateProduct(Product prod)
     {
-        int index = -1;
-        for ( int i = 0; i < products.size(); i++ )
-            if(products.get(i).getProductId() == prod.getProductId())
-                index = i;
-
-        if (index == -1)
-            return new  Product(0, "No Product Found", 0);
-
-        System.out.println(prod + " " + index);
-        return products.set(index, prod);
+//        int index = -1;
+//        for ( int i = 0; i < products.size(); i++ )
+//            if(products.get(i).getProductId() == prod.getProductId())
+//                index = i;
+//
+//        if (index == -1)
+//            return new  Product(0, "No Product Found", 0);
+//
+//        System.out.println(prod + " " + index);
+//        return products.set(index, prod);
+        return prodRepo.save(prod);
     }
 
     public Product deleteProduct(int prodId)
     {
-        int index = -1;
-        for ( int i = 0; i < products.size(); i++ )
-            if(products.get(i).getProductId() == prodId)
-                index = i;
+//        int index = -1;
+//        for ( int i = 0; i < products.size(); i++ )
+//            if(products.get(i).getProductId() == prodId)
+//                index = i;
+//
+//        if (index == -1)
+//            return new  Product(0, "No Product Found", 0);
+//
+//        return products.remove(index);
+        Product prod = prodRepo.findById(prodId).orElse(new Product());
+        prodRepo.deleteById(prodId);
+        return  prod;
 
-        if (index == -1)
-            return new  Product(0, "No Product Found", 0);
-
-        return products.remove(index);
     }
 }
