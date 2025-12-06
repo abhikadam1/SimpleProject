9189eb5ff357234b5f69de88d3abda56f5f92d2d
Abhi Kadam <kadamabhishek3188@gmail.com>
2025-12-06 21:25:43 +0530

    taking pull from main

--- src/main/java/com/example/SimpleProject/SimpleProjectApplication.java
+++ src/main/java/com/example/SimpleProject/SimpleProjectApplication.java
@@ -2,12 +2,22 @@ package com.example.SimpleProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SimpleProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleProjectApplication.class, args);
+        var context =SpringApplication.run(SimpleProjectApplication.class, args);
+
+        // Get environment
+        Environment env = context.getEnvironment();
+
+        // Read port (default 8080)
+        String port = env.getProperty("server.port", "8080");
+
+        // Print URL
+        System.out.println("\n🚀 Application running at: http://localhost:" + port + "\n");
 	}
 
 }
